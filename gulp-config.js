f43942da3731d646b70c1e8e90d2d49ad34f7c84
Jagan <s.jagannathan@yahoo.com>
2016-10-06 16:24:03 +0530

    initial commit

---
+++ gulp-config.js
@@ -0,0 +1,12 @@
+module.exports = function () {
+  var instanceRoot = "d:\\inetpub\\wwwroot\\sc81u3";
+  var config = {
+    websiteRoot: instanceRoot + "\\Website",
+    sitecoreLibraries: instanceRoot + "\\Website\\bin",
+    licensePath: instanceRoot + "\\Data\\license.xml",
+    solutionName: "Habitat",
+    buildConfiguration: "Debug",
+    runCleanBuilds: false
+  };
+  return config;
+}
---
+++ src/Foundation/Testing/tests/Attributes/ResolvePipelineAttribute.cs
@@ -0,0 +1,18 @@
+﻿namespace Sitecore.Foundation.Testing.Attributes
+{
+  using System;
+
+  public class ResolvePipelineAttribute : Attribute
+  {
+    public string PipelineName { get; set; }
+
+    public ResolvePipelineAttribute(string pipelineName)
+    {
+      this.PipelineName = pipelineName;
+    }
+
+    public ResolvePipelineAttribute()
+    {
+    }
+  }
+}
